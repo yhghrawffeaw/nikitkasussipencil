4867fefbc3e0205bbfafd08b772b4731c9ddfa58
yhghrawffeaw <yhghshyn@gmail.com>
2025-10-16 10:36:10 +0300

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sisesta täht ja vajuta enter");
+
+                string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "a":
+                    Console.WriteLine("1");
+                    break;
+
+                case "e":
+                    Console.WriteLine("2");
+                        break;
+
+                case "i":
+                    Console.WriteLine("3");
+                        break;
+
+                default:
+                    Console.WriteLine("4");
+                    break;
+            }
+        }
+    }
+}
